a132d9e52a7a92595f794ce7555d09bb9f51d451
Moumou Ding <xding3@ncsu.edu>
2017-10-05 17:45:00 -0400

    Upload Original Files

---
+++ test.js
@@ -0,0 +1,19 @@
+var subject = require('./subject.js')
+var mock = require('mock-fs');
+subject.inc('',undefined);
+subject.weird('','','',"strict");
+mock({"path/fileExists":{},"pathContent":{"file1":"text content"}});
+	subject.fileTest('path/fileExists','pathContent/file1');
+mock.restore();
+mock({"pathContent":{"file1":"text content"}});
+	subject.fileTest('path/fileExists','pathContent/file1');
+mock.restore();
+mock({"path/fileExists":{}});
+	subject.fileTest('path/fileExists','pathContent/file1');
+mock.restore();
+mock({});
+	subject.fileTest('path/fileExists','pathContent/file1');
+mock.restore();
+subject.normalize('');
+subject.format('','','');
+subject.blackListNumber('');
